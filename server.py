39d88926bacc5050fbe8e9775a3acc5863cd8e0b
glaslos <lukas.rist@bluecoat.com>
2015-01-29 10:45:01 +0100

    asyncio instead of tornado

--- server.py
+++ server.py
@@ -1,54 +1,26 @@
 #!/usr/bin/python3
 
-import os
-
-from uuid import uuid4
-
-import tornado.httpserver
-import tornado.ioloop
-import tornado.netutil
-import tornado.web
-import tornado.platform.asyncio
-
 import asyncio
+from aiohttp import web
 
 import handlers
-from utils import create_self_signed_cert
 
 
-CERT_FILE = 'selfsigned.crt'
-KEY_FILE = 'private.key'
-if not len(set(os.listdir('.')).intersection([KEY_FILE, CERT_FILE])) == 2:
-    create_self_signed_cert(CERT_FILE, KEY_FILE)
+@asyncio.coroutine
+def init(event_loop):
+    app = web.Application(loop=event_loop)
+    app.router.add_route('GET', '/projects', handlers.ProjectHandler(event_loop).handle_projects)
+    app.router.add_route('GET', '/projects_data', handlers.ProjectsDataHandler(event_loop).handle_projects_data)
+    app.router.add_route('GET', '/', handlers.IndexHandler(event_loop).handle_index)
+    app.router.add_route('GET', '/static/{folder}/{file_name}', handlers.StaticHandler(event_loop).handle_static)
 
+    srv = yield from event_loop.create_server(app.make_handler(), '127.0.0.1', 8080)
+    print("Server started at http://127.0.0.1:8080")
+    return srv
 
-class Application(tornado.web.Application):
-    def __init__(self, handlers, **settings):
-        tornado.web.Application.__init__(self, handlers, **settings)
-        self.gh_client_id = ''
-        self.gh_client_secret = ''
-        self.gh_state = str(uuid4())
-
-
-if __name__ == '__main__':
-    tornado.platform.asyncio.AsyncIOMainLoop().install()
-    app = Application(
-        [
-            (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static/'}),
-            (r'/', handlers.MainHandler),
-            (r'/ack', handlers.AckHandler),
-            (r'/syn', handlers.SynHandler),
-            (r'/issues', handlers.IssuesHandler),
-        ],
-        cookie_secret=str(uuid4()),
-        #login_url='/login',
-        #xsrf_cookies=True
-    )
-    sockets = tornado.netutil.bind_sockets(8080)
-    server = tornado.httpserver.HTTPServer(app, ssl_options={
-        'certfile': CERT_FILE,
-        'keyfile': KEY_FILE,
-    })
-    server.add_sockets(sockets)
-
-    asyncio.get_event_loop().run_forever()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
